37cf83a4f99ef2c3a631c395e9dafce5af3aee66
NerdyDave2017 <nerdydave2017@gmail.com>
2025-03-21 09:15:09 +0100

    feat: create interface to select no of people

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,18 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
     
-    let students = ["Harry", "Hermione", "Ron"]
+    let tipPercentages = [0, 5, 10, 15, 20, 25]
     
     var body: some View {
-        VStack{
-            Image(systemName: "globe")
-                .imageScale(.large)
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100, id: \.self) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
                 
-            Text("Hello, World!")
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
         }
-
     }
+
+    
 }
 
 #Preview {
